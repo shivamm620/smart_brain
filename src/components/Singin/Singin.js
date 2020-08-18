d8df943b3cd5122a8d5e5ab8475221522c82daa8
Shivam Mishra <shivammishra7763914693@gmail.com>
2020-08-18 23:51:26 +0530

    bad response

--- src/components/Singin/Singin.js
+++ src/components/Singin/Singin.js
@@ -1,35 +1,72 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Singin = ({onChangeRoute})=>{
+class Singin extends Component{
+  constructor(props){
+    super(props);
+    this.state={
+      singInEmail:'',
+      singInPassword:''
+    }
+  }
+  onEmailChange = (event) =>{
+    this.setState({singInEmail:event.target.value})
+  }
+  onPasswordChange = (event) =>{
+    this.setState({singInPassword:event.target.value})
+  }
+  onSubmitSingIn = () =>{
+    fetch('http://localhost:6000/singin' , {
+      method: 'post',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({
+        email : this.state.singInEmail,
+        password: this.state.singInPassword
+      })
+    })
+    this.props.onChangeRoute('home')
+  }
+  render(){
+    const {onChangeRoute} = this.props;
     return(
-        <div>
-        <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
-        <main className="pa4 black-80">         
-             <form className="measure">
-               <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
-                 <legend className="f1 fw6 ph0 mh0">Sign In</legend>
-                 <div className="mt3">
-                   <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
-                   <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address"  id="email-address"/>
-                 </div>
-                 <div className="mv3">
-                   <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
-                   <input className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="password" name="password"  id="password"/>
-                 </div>
-
-               </fieldset>
-               <div className="">
-                 <input onClick={ ()=>onChangeRoute('home')} className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" type="submit" value="Sign in"/>
+      <div>
+      <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
+      <main className="pa4 black-80">         
+           <form className="measure">
+             <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
+               <legend className="f1 fw6 ph0 mh0">Sign In</legend>
+               <div className="mt3">
+                 <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
+                 <input 
+                 className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
+                 type="email"
+                  name="email-address"  
+                  onChange={this.onEmailChange}
+                  id="email-address"/>
                </div>
-               <div className="lh-copy mt3">
-                 <p  onClick={ ()=>onChangeRoute('register')} className="f6 link dim black db pointer">Register</p>
+               <div className="mv3">
+                 <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
+                 <input 
+                 className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
+                 type="password" 
+                 name="password" 
+                 onChange={this.onPasswordChange}
+                  id="password"/>
                </div>
-             </form>
-            </main>
-            </article>
 
-        </div>
-    )
+             </fieldset>
+             <div className="">
+               <input onClick={this.onSubmitSingIn} className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" type="submit" value="Sign in"/>
+             </div>
+             <div className="lh-copy mt3">
+               <p  onClick={ ()=>onChangeRoute('register')} className="f6 link dim black db pointer">Register</p>
+             </div>
+           </form>
+          </main>
+          </article>
+
+      </div>
+  )
+  }
 }
 
 export default Singin ;
